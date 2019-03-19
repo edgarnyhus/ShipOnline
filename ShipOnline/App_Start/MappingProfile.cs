9e183094255d7d89a30d6cf41a48851a78b88508
edgarnyhus <edgar.nyhus@gmail.com>
2019-03-19 13:09:44 +0100

    Added Data Transfer Object  to CompaniesController - first step

---
+++ ShipOnline/App_Start/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using AutoMapper.Mappers;
+using ShipOnline.Core.Domain;
+using ShipOnline.Dtos;
+
+namespace ShipOnline.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Company, CompanyDto>();
+            CreateMap<CompanyDto, Company>();
+        }
+    }
+}
--- ShipOnline/Controllers/Api/CompaniesController.cs
+++ ShipOnline/Controllers/Api/CompaniesController.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web.Configuration;
 using System.Web.Http;
+using AutoMapper;
 using ShipOnline.Core;
 using ShipOnline.Core.Domain;
 using ShipOnline.Persistence;
+using ShipOnline.Dtos;
 
 namespace ShipOnline.Controllers.Api
 {
@@ -14,77 +17,86 @@ namespace ShipOnline.Controllers.Api
     {
         private readonly IUnitOfWork _unitOfWork;
 
-        public CompaniesController()
-        {
-            _unitOfWork = new UnitOfWork(new ShipBaseDbContext());
-        }
+        // Using Data Transfer Object
+
+        //public CompaniesController()
+        //{
+        //    _unitOfWork = new UnitOfWork(new ShipBaseDbContext());
+        //}
         public CompaniesController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
         }
 
         // GET /api/companies
-        public IEnumerable<Company> GetCompanies()
+        [HttpGet]
+        public IEnumerable<CompanyDto> GetCompanies()
         {
-            return _unitOfWork.Companies.GetAll().ToList();
+            List<Company> result = _unitOfWork.Companies.GetAll().ToList();
+            return result.Select(Mapper.Map<Company, CompanyDto>);
         }
 
-        // GET /api/companies/1
-        public Company GetCompany(string id)
+        // Return Data Transfer Object
+        [HttpGet]
+        public CompanyDto GetCompanyDto(string id)
         {
-            Company company = _unitOfWork.Companies.SingleOrDefault(c => c.ID == id);
+            var company = _unitOfWork.Companies.SingleOrDefault(c => c.ID == id);
             if (company == null)
-            {
                 throw new HttpResponseException(HttpStatusCode.NotFound);
-            }
 
-            return company;
+            return Mapper.Map<Company, CompanyDto>(company);
         }
 
         // POST /api/companies
         [HttpPost]
-        public Company CreateCompany(Company company)
+        public CompanyDto CreateCompany(CompanyDto companyDto)
         {
             if (!ModelState.IsValid)
                 throw new HttpResponseException(HttpStatusCode.BadRequest);
 
+            var company = Mapper.Map<CompanyDto, Company>(companyDto);
             _unitOfWork.Companies.Add(company);
             _unitOfWork.Complete();
-            return company;
+
+            companyDto.ID = company.ID;
+            return companyDto;
         }
 
         // PUT /api/companies/1
         [HttpPut]
-        public void UpdateCompany(string id, Company company)
+        public void UpdateCompany(string id, CompanyDto companyDto)
         {
             if (!ModelState.IsValid)
                 throw new HttpResponseException(HttpStatusCode.BadRequest);
 
             Company result = _unitOfWork.Companies.SingleOrDefault((c => c.ID == id));
             if (result == null)
-                throw  new HttpResponseException(HttpStatusCode.NotFound);
+                throw new HttpResponseException(HttpStatusCode.NotFound);
 
             // Use Automaper to update result
+            //var company = Mapper.Map<CompanyDto, Company>(companyDto, result);
+            var company = Mapper.Map(companyDto, result);
 
             _unitOfWork.Complete();
         }
 
         // PUT /api/companies/1
         [HttpDelete]
-        public void DeleteCompany(Company company)
+        public void DeleteCompany(CompanyDto companyDto)
         {
             if (!ModelState.IsValid)
                 throw new HttpResponseException(HttpStatusCode.BadRequest);
 
             // Check if companu exists
-            Company result = _unitOfWork.Companies.SingleOrDefault((c => c.ID == company.ID));
-            if (result == null)
+            Company company = _unitOfWork.Companies.SingleOrDefault((c => c.ID == companyDto.ID));
+            if (company == null)
                 throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            Mapper.Map(companyDto, company);
 
             _unitOfWork.Companies.Remove(company);
             _unitOfWork.Complete();
         }
-
     }
 }
  
